9b814e4a44f939c6db02aa354161cb82bb192f12
Doug Fairfield <fairfielddouglas@gmail.com>
2024-12-14 13:30:48 +0000

    updated bird_sightings model in line with sql file

--- backend/tests/test_sightings.py
+++ backend/tests/test_sightings.py
@@ -1,22 +1,23 @@
+from datetime import datetime
 from lib.models.sightings import Sighting
 
 """
 Sighting constructs with a bird name,  and password
 """
 def test_sightings_constructs():
-    user = Sighting(1, "eagle", "2024-03-22", "eagle_recipe", "recipe_123")
+    user = Sighting(1, "eagle", "2024-03-22", "London", 1)
     assert user.id == 1
     assert user.bird_name == "eagle"
-    assert user.date_spotted == "2024-03-22"
-    assert user.bird_recipe == "eagle_recipe"
-    assert user.bird_recipe_id == "recipe_123"
+    assert user.date_spotted.isoformat() == "2024-03-22"
+    assert user.location == "London"
+    assert user.user_id == 1
 
 """
 We can format users to strings nicely
 """
 def test_sighting_format_nicely():
-    sightings = Sighting(1, "eagle", "2024-03-22", "eagle_recipe", "recipe_123")
-    assert str(sightings) == "User(1, eagle, 2024-03-22, eagle_recipe, recipe_123)"
+    sightings = Sighting(1, "eagle", "2024-03-22", "London", 1)
+    assert str(sightings) == "User(1, eagle, 2024-03-22, London, 1)"
 
 
 """
@@ -24,6 +25,6 @@ We can compare two identical users
 And have them be equal
 """
 def test_sightings_are_equal():
-    Sightings1 = Sighting(1, "eagle", "2024-03-22", "eagle_recipe", "recipe_123")
-    Sightings2 = Sighting(1, "eagle", "2024-03-22", "eagle_recipe", "recipe_123")
+    Sightings1 = Sighting(1, "eagle", "2024-03-22", "London", 1)
+    Sightings2 = Sighting(1, "eagle", "2024-03-22", "London", 1)
     assert Sightings1 == Sightings2
