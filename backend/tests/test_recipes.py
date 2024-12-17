d7ff2e8ab24cc52b104b60944cc5f82eb80fef7d
Max Joseph <maxjoseph22@gmail.com>
2024-12-17 12:01:14 +0000

    recipes repo changed etc

--- backend/tests/test_recipes.py
+++ backend/tests/test_recipes.py
@@ -1,40 +1,31 @@
 from lib.models.recipes import Recipe
-"""
-Recipe constructs with an id, title, date_created, recipe_rating, cooking_time, user_id
-"""
+
 def test_recipe_constructs():
-    recipe = Recipe(1, 'Blue Jay Roast', '2024-12-01', 5, 45, 1)
+    recipe = Recipe(1, 'Blue Jay Roast', '2024-12-01', 45, 1, avg_rating=5)
     assert recipe.id == 1
     assert recipe.title == 'Blue Jay Roast'
     assert recipe.date_created == '2024-12-01'
-    assert recipe.recipe_rating == 5
     assert recipe.cooking_time == 45
+    assert recipe.bird_sighting_id == 1
+    assert recipe.avg_rating == 5
 
 def test_recipe_to_dict():
-    recipe = Recipe(1, 'Blue Jay Roast', '2024-12-01', 5, 45, 1)
+    recipe = Recipe(1, 'Blue Jay Roast', '2024-12-01', 45, 1, avg_rating=5)
     expected_dict = {
-    "id": 1,
-    "title": 'Blue Jay Roast',
-    "date_created": '2024-12-01',
-    "recipe_rating": 5,
-    "cooking_time": 45,
-    "bird_sighting_id": 1
-}
+        "id": 1,
+        "title": 'Blue Jay Roast',
+        "date_created": '2024-12-01',
+        "cooking_time": 45,
+        "bird_sighting_id": 1,
+        "avg_rating": 5
+    }
     assert recipe.to_dict() == expected_dict
 
-"""
-We can format recipes to strings nicely
-"""
 def test_recipe_formats_nicely():
-    recipe = Recipe(1, 'Blue Jay Roast', '2024-12-01', 5, 45, 1)
-    assert str(recipe) == "Recipe(1, Blue Jay Roast, 2024-12-01, 5, 45, 1)"
-    
-"""
-We can compare two identical users
-And have them be equal
-"""
+    recipe = Recipe(1, 'Blue Jay Roast', '2024-12-01', 45, 1, avg_rating=5)
+    assert str(recipe) == "Recipe(1, Blue Jay Roast, 2024-12-01, 45, 1, 5)"
+
 def test_recipes_are_equal():
-    recipe1 = Recipe(1, 'Blue Jay Roast', '2024-12-01', 5, 45, 1)
-    recipe2 = Recipe(1, 'Blue Jay Roast', '2024-12-01', 5, 45, 1)
+    recipe1 = Recipe(1, 'Blue Jay Roast', '2024-12-01', 45, 1, avg_rating=5)
+    recipe2 = Recipe(1, 'Blue Jay Roast', '2024-12-01', 45, 1, avg_rating=5)
     assert recipe1 == recipe2
-    
