204297290ba57b37d55ddae0a7dcea99103b9de2
Russell Coles <russell.o.coles@gmail.com>
2024-12-19 17:02:09 +0000

    penultimate final styling

--- frontend/src/pages/Team.jsx
+++ frontend/src/pages/Team.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "./Team.css";
+import { Navbar } from "../components/Navbar";
 
 export function Team() {
   return (
-    <div className="team-container">
-      <h1>Meet the team</h1>
-      <h3>John O'Neill</h3>
-      <h3>Russell Coles</h3>
-      <h3>Alberto Tobara</h3>
-      <h3>Max Joseph</h3>
-      <h3>Ben Cole</h3>
-      <h3>Doug Fairfield</h3>
-    </div>
+    <>
+    <Navbar />
+      <div className="team-container">
+        <h1>Meet the team</h1>
+        <h3>Ben Cole</h3>
+        <h3>Russell Coles</h3>
+        <h3>Doug Fairfield</h3>
+        <h3>Max Joseph</h3>
+        <h3>John O'Neill</h3>
+        <h3>Alberto Tobara</h3>
+      </div>
+    </>
   );
 }
